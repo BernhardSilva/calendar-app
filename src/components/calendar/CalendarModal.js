f22d4da1952ad0a8f5f36048508a69b819551eab
Bernhard Silva <bernhard.silva@gmail.com>
2021-04-14 17:45:33 -0400

    calendar modal form added, date validations pending

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
 import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment';
+import { useState } from 'react';
 
 const customStyles = {
   content: {
@@ -14,17 +16,59 @@ const customStyles = {
 
 Modal.setAppElement('#root');
 
+const now = moment().minutes(0).seconds(0).add(1, 'hours'); //16:00
+const last = now.clone().add(1, 'hours');
+
 export const CalendarModal = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const handleInputChange = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
+
+  const [startDate, setStartDate] = useState(now.toDate());
+  const [lastDate, setLastDate] = useState(last.toDate());
+
+  const [formValues, setFormValues] = useState({
+    title: 'Evento',
+    notes: '',
+    start: now.toDate(),
+    end: last.toDate(),
+  });
+
+  const { notes, title } = formValues;
 
   const closeModal = () => {
     console.log('closing...');
-    setIsOpen(false);
+  };
+
+  const handleStartDateChange = (e) => {
+    console.log(e);
+    setStartDate(e);
+    setFormValues({
+      ...formValues,
+      start: e,
+    });
+  };
+
+  const handleLastDateChange = (e) => {
+    console.log(e);
+    setLastDate(e);
+    setFormValues({
+      ...formValues,
+      end: e,
+    });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault(); //avoid form spread
+    console.log(formValues);
   };
 
   return (
     <Modal
-      isOpen={isOpen}
+      isOpen={true}
       // onAfterOpen={afterOpenModal}
       onRequestClose={closeModal}
       style={customStyles}
@@ -32,9 +76,65 @@ export const CalendarModal = () => {
       className="modal"
       overlayClassName="modal-background"
     >
-      <h1>Calendar Modal</h1>
+      <h1> Nuevo evento </h1>
       <hr />
-      <span>Hola de nuevo</span>
+      <form className="container" onSubmit={handleSubmitForm}>
+        <div className="form-group">
+          <label>Fecha y hora inicio</label>
+          <DateTimePicker
+            onChange={handleStartDateChange}
+            value={startDate}
+            className="form-control react-datetimepicker react-datetimepicker__wrapper"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Fecha y hora fin</label>
+          <DateTimePicker
+            onChange={handleLastDateChange}
+            value={lastDate}
+            minDate={startDate}
+            className="form-control react-datetimepicker react-datetimepicker__wrapper"
+          />
+        </div>
+
+        <hr />
+        <div className="form-group">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+            value={title}
+            onChange={handleInputChange}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className="form-group">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={notes}
+            onChange={handleInputChange}
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
   );
 };
