36251931bf828bbe66ed78669f2e4ad8b2ac6a0f
Bernhard Silva <bernhard.silva@gmail.com>
2021-04-16 16:47:25 -0400

    logo added to navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,7 +1,11 @@
+import logo from '../../assets/logo.png';
+
 export const Navbar = () => {
   return (
     <div className="navbar navbar-dark bg-dark mb-4">
-      <span className="navbar-brand"> Bernhard</span>
+      <span className="navbar-brand">
+        <img src={logo} alt="logo" className="logo-navbar" />
+      </span>
 
       <button className="btn btn-secondary fab-logout" alt="Logout">
         <i className="fas fa-sign-out-alt pr-2 " />
